a3d47d6da4b1ca6c05fbce37b0436160b54b7d9f
Ran Ziv <ran@gigaspaces.com>
2014-01-14 13:46:52 +0200

    basic project structure

---
+++ cloudify_vagrant/cloudify_vagrant.py
@@ -0,0 +1,29 @@
+########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+__author__ = 'ran'
+
+
+def init(logger, target_directory, config_file_name,
+         defaults_config_file_name):
+    raise RuntimeError('NOT YET IMPLEMENTED')
+
+
+def bootstrap(logger, config, management_ip=None):
+    raise RuntimeError('NOT YET IMPLEMENTED')
+
+
+def teardown(logger, management_ip):
+    raise RuntimeError('NOT YET IMPLEMENTED')
